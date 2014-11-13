4dda71a613823168c51c508fef96986f456d73d0
smothiki <sivaram.mothiki@gmail.com>
2014-11-12 23:13:02 -0500

    phase 3 final version

--- distalgo/clientserver.py
+++ distalgo/clientserver.py
@@ -32,64 +32,60 @@ class Master(da.DistProcess):
 
     def setup(self, servers, client, Nlength):
         self.client = client
-        self.Nlength = Nlength
         self.servers = servers
+        self.Nlength = Nlength
         self.serverisAlive = {}
         self.head = 0
         self.tail = (self.Nlength - 1)
         for (i, s) in enumerate(list(self.servers)):
-            self.serverisAlive[i] = 0
+            self.serverisAlive[i] = 1
 
     def main(self):
         self.output(('%r.' % self.serverisAlive))
-        _thread.start_new_thread(self.checkHealthmsg, ('Thread', 15))
+        _thread.start_new_thread(self.checkHealthmsg, ('Thread', 6))
         _thread.start_new_thread(self.messuphead, ('Thread1', 4))
-        _thread.start_new_thread(self.messuptail, ('Thread2', 7))
-        _thread.start_new_thread(self.messupserver, ('Thread3', 1, 9))
-        _st_label_53 = 0
-        while (_st_label_53 == 0):
-            _st_label_53 += 1
+        _thread.start_new_thread(self.messuptail, ('Thread2', 8))
+        _thread.start_new_thread(self.messupserver, ('Thread3', 1, 6))
+        _st_label_49 = 0
+        while (_st_label_49 == 0):
+            _st_label_49 += 1
             if False:
-                _st_label_53 += 1
+                _st_label_49 += 1
             else:
-                super()._label('_st_label_53', block=True)
-                _st_label_53 -= 1
+                super()._label('_st_label_49', block=True)
+                _st_label_49 -= 1
 
     def checkHealthmsg(self, threadName, delay):
         while True:
-            time.sleep(delay)
-            self.output(('moon %r.' % self.serverisAlive))
-            for (i, s) in enumerate(list(self.servers)):
+            time.sleep(1)
+            self.output(('state %r.' % self.serverisAlive))
+            for (i, s) in enumerate(self.servers):
                 if (self.serverisAlive[i] == 0):
                     if (i == self.head):
-                        self.output('here1')
+                        self.output('killing head')
                         newHead = (i + 1)
                         self._send(('ImHead', newHead), self.client)
                     elif (i == self.tail):
-                        self.output('here2')
+                        self.output('killing tail')
                         newTail = (i - 1)
-                        self._send(('ImTail', newTail), s[(i - 2)])
+                        self._send(('ImTail', newTail), self.servers[(i - 2)])
                         self._send(('ImTail', newTail), self.client)
                     else:
-                        self._send(('urnext', (i + 1)), s[(i - 1)])
+                        self._send(('urnext', (i + 1)), self.servers[(i - 1)])
 
     def messuphead(self, threadName, headkill):
         time.sleep(headkill)
-        self.output('killing head')
         self.serverisAlive[0] = 0
 
     def messuptail(self, threadName, tailkill):
         time.sleep(tailkill)
-        self.output('killing tail')
         self.serverisAlive[(self.Nlength - 1)] = 0
 
     def messupserver(self, threadName, index, timetokill):
         time.sleep(timetokill)
-        self.output('killing server')
         self.serverisAlive[index] = 0
 
-    def _Master_handler_0(self, serverId, p):
-        self.output(('healthcheck received%r.' % serverId))
+    def _Master_handler_0(self, p, serverId):
         self.serverisAlive[serverId] = 1
     _Master_handler_0._labels = None
     _Master_handler_0._notlabels = None
@@ -102,11 +98,11 @@ class Server(da.DistProcess):
         self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_0', PatternExpr_2, sources=[PatternExpr_3], destinations=None, timestamps=None, record_history=True, handlers=[]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_1', PatternExpr_5, sources=[PatternExpr_6], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_1]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_2', PatternExpr_7, sources=[PatternExpr_8], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_2]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_3', PatternExpr_9, sources=[PatternExpr_10], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_3])])
 
     def setup(self, servers, index, client, Nlength, master):
-        self.index = index
-        self.master = master
         self.client = client
-        self.Nlength = Nlength
         self.servers = servers
+        self.index = index
+        self.Nlength = Nlength
+        self.master = master
         self.bankobj = Bank('wells', '123')
         self.startindex = 0
         self.head = self.servers[0]
@@ -119,32 +115,32 @@ class Server(da.DistProcess):
         self.output(('serverindex' + str(self.index)))
 
     def main(self):
-        _thread.start_new_thread(self.sendHealthCheck, ('Thread', 5, self.index, self.master))
-        request = p = None
+        _thread.start_new_thread(self.sendHealthCheck, ('Thread', 3, self.index, self.master))
+        p = request = None
 
         def ExistentialOpExpr_0():
-            nonlocal request, p
+            nonlocal p, request
             for (_, (_, _, p), (_ConstantPattern15_, request)) in self._ServerReceivedEvent_0:
                 if (_ConstantPattern15_ == 'request'):
                     if True:
                         return True
             return False
-        _st_label_67 = 0
-        while (_st_label_67 == 0):
-            _st_label_67 += 1
+        _st_label_63 = 0
+        while (_st_label_63 == 0):
+            _st_label_63 += 1
             if ExistentialOpExpr_0():
-                _st_label_67 += 1
+                _st_label_63 += 1
             else:
-                super()._label('_st_label_67', block=True)
-                _st_label_67 -= 1
-        _st_label_68 = 0
-        while (_st_label_68 == 0):
-            _st_label_68 += 1
+                super()._label('_st_label_63', block=True)
+                _st_label_63 -= 1
+        _st_label_64 = 0
+        while (_st_label_64 == 0):
+            _st_label_64 += 1
             if False:
-                _st_label_68 += 1
+                _st_label_64 += 1
             else:
-                super()._label('_st_label_68', block=True)
-                _st_label_68 -= 1
+                super()._label('_st_label_64', block=True)
+                _st_label_64 -= 1
 
     def sendHealthCheck(self, threadName, delay, index, master):
         self.output('inside sendHealthCheck')
@@ -153,13 +149,13 @@ class Server(da.DistProcess):
             time.sleep(delay)
             self._send(('HealthCheckmsg', index), master)
 
-    def _Server_handler_1(self, p, tindex):
+    def _Server_handler_1(self, tindex, p):
         self.output('updated tail server end')
         self.endindex = tindex
     _Server_handler_1._labels = None
     _Server_handler_1._notlabels = None
 
-    def _Server_handler_2(self, p, tindex):
+    def _Server_handler_2(self, tindex, p):
         self.output('updated next server end')
         self.next = self.servers[tindex]
     _Server_handler_2._labels = None
@@ -178,7 +174,6 @@ class Server(da.DistProcess):
             self._send(('request', request), self.next)
             self.output(('<%r,%s,%r>' % (request.requestid, request.outcome, request.balance)))
         else:
-            self.output('sending reply to client')
             self._send(('reply', request), self.client)
     _Server_handler_3._labels = None
     _Server_handler_3._notlabels = None
@@ -190,9 +185,9 @@ class Client(da.DistProcess):
         self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_0', PatternExpr_11, sources=[PatternExpr_12], destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_4]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_1', PatternExpr_13, sources=[PatternExpr_14], destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_5]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_2', PatternExpr_15, sources=[PatternExpr_16], destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_6])])
 
     def setup(self, chain, master, Nlength):
-        self.master = master
-        self.chain = chain
         self.Nlength = Nlength
+        self.chain = chain
+        self.master = master
         self.waitfor = set()
         self.requests = Requests(3, 'getbalance')
         self.requests.getRequestList()
@@ -205,33 +200,34 @@ class Client(da.DistProcess):
             if (request.requestid in self.waitfor):
                 self.waitfor.remove(request.requestid)
             self.waitfor.add(request.requestid)
-            time.sleep(2)
+            time.sleep(1)
             if (request.transaction == 'getbalance'):
                 self._send(('request', request), self.tail)
             else:
                 self._send(('request', request), self.head)
-        _st_label_112 = 0
+        _st_label_107 = 0
         self._timer_start()
-        while (_st_label_112 == 0):
-            _st_label_112 += 1
+        while (_st_label_107 == 0):
+            _st_label_107 += 1
             if False:
                 pass
-                _st_label_112 += 1
+                _st_label_107 += 1
             elif self._timer_expired:
                 self.output('timeout')
-                _st_label_112 += 1
+                _st_label_107 += 1
             else:
-                super()._label('_st_label_112', block=True, timeout=20)
-                _st_label_112 -= 1
+                super()._label('_st_label_107', block=True, timeout=40)
+                _st_label_107 -= 1
         self.output('finished request')
 
-    def _Client_handler_4(self, p, index):
+    def _Client_handler_4(self, index, p):
         self.output('new head updated')
         self.head = self.chain[index]
     _Client_handler_4._labels = None
     _Client_handler_4._notlabels = None
 
-    def _Client_handler_5(self, index, p):
+    def _Client_handler_5(self, p, index):
+        self.output('new head updated')
         self.tail = self.chain[index]
     _Client_handler_5._labels = None
     _Client_handler_5._notlabels = None
