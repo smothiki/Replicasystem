3ee992b3c6f6be7d0325a7b4950318b70ec55e3d
sluck2013 <yswang2013@gmail.com>
2014-11-25 14:39:26 -0500

    testcase 16, start.go?

--- src/start/start.go
+++ src/start/start.go
@@ -15,7 +15,7 @@ func exe_cmd(cmd string, wg *sync.WaitGroup) {
 	parts := strings.Fields(cmd)
 	out, err := exec.Command(parts[0], parts[1], parts[2]).Output()
 	if err != nil {
-		fmt.Println("error occured")
+		fmt.Println("error occured when starting processes")
 		fmt.Printf("%s", err)
 	}
 	fmt.Printf("%s", out)
@@ -24,13 +24,12 @@ func exe_cmd(cmd string, wg *sync.WaitGroup) {
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run start.go <config file>")
+		fmt.Println("Usage: go run start/start.go <config file>")
 		return
 	}
 	logPath := os.Getenv("GOPATH") + "/src/github.com/replicasystem/logs/"
-	os.Remove(logPath + "mlogs")
-	os.Remove(logPath + "slogs")
-	os.Remove(logPath + "clogs")
+	os.RemoveAll(logPath)
+	os.Mkdir(logPath, 0777)
 
 	utils.SetConfigFile(os.Args[1])
 	totalchains, _ := strconv.Atoi(utils.Getconfig("chains"))
@@ -41,24 +40,26 @@ func main() {
 	fmt.Println(totalchains)
 	wg := new(sync.WaitGroup)
 	commands := make([]string, 0, 1)
-	master := utils.GetBinDir() + "master " + os.Args[1] + " 1"
-	commands = append(commands, master)
 	for i := 0; i < totalchains; i++ {
 		//start servers
 		curSeries := 1000 * (series + i)
 		for start := curSeries + 1; start <= curSeries+lenservers; start++ {
 			strin := utils.GetBinDir() + "server " + strconv.Itoa(start) + " " + os.Args[1]
 			commands = append(commands, strin)
-
 		}
+	}
 
+	master := utils.GetBinDir() + "master " + os.Args[1] + " 1"
+	commands = append(commands, master)
+
+	for i := 0; i < totalchains; i++ {
+		curSeries := 1000 * (series + i)
 		//start clients
 		for start := curSeries + 999; start > curSeries+999-clientNum; start-- {
 
 			client := utils.GetBinDir() + "client " + strconv.Itoa(start) + " " + os.Args[1]
 			commands = append(commands, client)
 		}
-		series = series + 1
 	}
 	// commands := []string{"/Users/ram/deistests/bin/server 4001", "/Users/ram/deistests/bin/server 4002", "/Users/ram/deistests/bin/server 4003"}
 	fmt.Println(len(commands))
